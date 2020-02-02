76f4ed31d9ce07085a6ba9339bd505b7186b2b46
= <diyar.mohammadi@ut.ac.ir>
2020-02-02 05:58:52 +0330

    added everything :)))

--- source.py
+++ source.py
@@ -108,4 +108,88 @@ ready_test = pd.get_dummies(test[test.select_dtypes(object).columns], drop_first
 
 ready_test = add_missing_cols(ready_data, ready_test)
 
+train_size = 500000
 
+ready_data = ready_data.sample(len(ready_data))
+
+train_data = ready_data[:train_size]
+valid_data = ready_data[train_size:]
+
+train_y = train_data["Market Share_total"].values
+train_x_episode = train_data[ "Episode"].values
+train_x = train_data.drop(["Market Share_total", "Episode"], axis=1).values
+
+valid_y = valid_data["Market Share_total"].values
+valid_x_episode = valid_data[ "Episode"].values
+valid_x = valid_data.drop(["Market Share_total", "Episode"], axis=1).values
+
+test_x_episode = ready_test[ "Episode"].values
+test_x = ready_test.drop(["Episode"], axis=1).values
+
+
+MU = []
+Sigma = []
+for i in range(train_x.shape[1]):
+  MU.append(train_x.T[i].mean())
+  Sigma.append(train_x.T[i].std())
+  valid_x.T[i] = (valid_x.T[i] - MU[i])/Sigma[i]
+  train_x.T[i] = (train_x.T[i] - MU[i])/Sigma[i]
+  test_x.T[i]  = ( test_x.T[i] - MU[i])/Sigma[i]
+MU.append(train_y.mean())
+Sigma.append(train_y.std())
+valid_y = (valid_y - train_y.mean())/train_y.std()
+train_y = (train_y - train_y.mean())/train_y.std()
+
+#<----------------------------------------------------------->#
+
+m = train_y.mean()
+print("MAE of Using Mean is ", np.mean(np.abs(m - valid_y)))
+
+
+reg = LinearRegression().fit(train_x, train_y)
+print("MAE of linear regression is ", np.mean(np.abs(reg.predict(valid_x) - valid_y)))
+
+
+model1 = tf.keras.models.Sequential([
+  tf.keras.layers.Input(shape=(1,)),
+  tf.keras.layers.Embedding(vocab_size+1, 16),
+  tf.keras.layers.Flatten()
+])
+
+model2 = tf.keras.models.Sequential([
+  tf.keras.layers.Input(shape=(81,)),
+  tf.keras.layers.Dense(120, activation='relu')
+])
+
+mergedOut = tf.keras.layers.Concatenate()([model1.output, model2.output])
+mergedOut = tf.keras.layers.Flatten()(mergedOut)
+mergedOut = tf.keras.layers.Dropout(0.2)(mergedOut)
+mergedOut = tf.keras.layers.Dense(120, activation='relu')(mergedOut)
+mergedOut = tf.keras.layers.Dropout(0.2)(mergedOut)
+mergedOut = tf.keras.layers.Dense(1)(mergedOut)
+
+model = tf.keras.models.Model([model1.input, model2.input], mergedOut)
+                              
+model.compile(loss='mae',
+        optimizer="Adam",
+        metrics=[metrics.mae])
+model.summary()
+
+epochs = 50
+batch_size = 128
+history = model.fit([train_x_episode, train_x], train_y,
+    batch_size=batch_size,
+    epochs=epochs,
+    shuffle=True,
+    verbose=1, # Change it to 2, if wished to observe execution
+    validation_data=([valid_x_episode, valid_x], valid_y))
+
+y_pred = model.predict([valid_x_episode, valid_x])
+print("MAE of NN is ", np.mean(np.abs(y_pred.flatten() - valid_y)))
+print("R2 score of NN is ", r2_score(valid_y, y_pred.flatten()))
+
+
+
+pred_file_address = "drive/My Drive/pred_test.csv"
+y_pred_test = model.predict([test_x_episode, test_x])
+pd.DataFrame(y_pred * Sigma[-1] + MU[-1]).to_csv(pred_file_address)
